60e29e022e55543a7418e532c6dec2b996cf94eb
cristianmaldonado <maldonadocristian2010@hotmail.com>
2017-10-23 19:41:52 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatButtonModule, MatMenuModule, MatCardModule, MatIconModule, MatSidenavModule, MatToolbarModule } from "@angular/material";
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SideNavComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
